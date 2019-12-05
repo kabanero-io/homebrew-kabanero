c257908213afb301fbb983f8c9d5178b459c2949
Claudia Yan <claudia.yan@ibm.com>
2019-12-05 18:06:19 -0500

    missing in in sha?

--- kabanero.rb
+++ kabanero.rb
@@ -2,7 +2,7 @@ class Kabanero < Formula
   desc "kabanero cli"
   homepage "https://kabanero.io/"
   url "https://github.com/kabanero-io/kabanero-command-line/releases/download/0.4.0-rc.1/kabanero-0.4.0-rc.1-homebrew-amd64.tar.gz"
-  sha256 "3466cba537640ea58c980307f59230400bd03b08b82320a54e76076e4638acb"
+  sha256 "3466cba537640ea58c980307f59230400bd03b08b82320a54e76076e4638acbe"
 
 
   def install
